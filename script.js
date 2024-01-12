5cedee35b5bd8227097660f9503059e4a252ee7b
sukritaz <sukri.pani@gmail.com>
2024-01-12 22:22:11 +0530

    Able to add books to the library.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const myLibrary = [];
 
 const addBookButton = document.getElementById("add-book-button");
+const books = document.getElementById("booksGrid");
 
 addBookButton.addEventListener("click", () => {
     openAddBookModal();
@@ -29,8 +30,48 @@ function closeModal() {
 }
 
 function submitForm(event) {
-    // event.preventDefault();
+    event.preventDefault();
     let book = new Book(document.getElementById('bookTitle').value, document.getElementById('authorName').value);
-    book.sayName();
+    myLibrary.push(book);
+    updateLibrary();
+}
+
+function updateLibrary() {
+    createBookElement(myLibrary.slice(-1)[0]);
+}
+
+function createBookElement(book) {
+    const bookElem = document.createElement('div');
+    const bookNameElem = document.createElement('div');
+    const bookNameTitleElem = document.createElement('div');
+    const bookAuthorElem = document.createElement('div');
+    const bookAuthorTitleElem = document.createElement('div');
+    const readToggleElem = document.createElement('button');
+
+    bookElem.className = "book";
+    bookNameTitleElem.className = "bookTitleName"
+    bookNameElem.className = "bookName";
+    bookAuthorTitleElem.className = "bookAuthorTitle"
+    bookAuthorElem.className = "bookAuthor";
+    readToggleElem.id = "toggleRead";
+
+    bookNameTitleElem.textContent = "Name";
+    bookNameElem.textContent = book.name;
+    bookAuthorTitleElem.textContent = "Author";
+    bookAuthorElem.textContent = book.author;
+    readToggleElem.textContent = "READ"
+
+    readToggleElem.addEventListener("click", function() {
+        this.classList.toggle("active");
+    });
+    
+
+    bookElem.appendChild(bookNameTitleElem);
+    bookElem.appendChild(bookNameElem);
+    bookElem.appendChild(bookAuthorTitleElem)
+    bookElem.appendChild(bookAuthorElem);
+    bookElem.appendChild(readToggleElem);
+
+    books.appendChild(bookElem);
 }
 
